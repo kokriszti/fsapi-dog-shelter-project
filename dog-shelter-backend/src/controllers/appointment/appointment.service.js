651edc130e082322ed4951af41d02356fb08f6ac
kokriszti <kokriszti@gmail.com>
2022-05-30 12:17:27 +0200

    appointment populate done

--- dog-shelter-backend/src/controllers/appointment/appointment.service.js
+++ dog-shelter-backend/src/controllers/appointment/appointment.service.js
@@ -4,13 +4,15 @@ const Appointment = require("../../models/appointment.model")
 //READ:
 exports.findAll = () => {
     return Appointment.find()
+        .populate("dog", {name: 1, _id: 0})
+        .populate("user", {"adoptionForm.firstName": 1, "adoptionForm.lastName": 1, _id: 0})
 }
 
 //READ BY ID:
 exports.findOne = id => {
     return Appointment.findById(id)
-      //.populate("dogs", {name: 1, _id: 0})          //toDo: ezzel a sorral megnézni
-      //.populate("users", {adoptionForm.firstName: 1, adoptionForm.lastName: 1, _id: 0})       //toDo: ezzel a sorral megnézni
+      .populate("dog", {name: 1, _id: 0})
+      .populate("user", {"adoptionForm.firstName": 1, "adoptionForm.lastName": 1, _id: 0})
 }
 
 //CREATE:
